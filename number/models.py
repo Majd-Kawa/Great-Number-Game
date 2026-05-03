8047f9893881a3bdbcf3dd8f4ca077837b0b65a1
Majd Kawa <majd.mkawa@gmail.com>
2026-05-03 20:08:04 +0300

    Add leaderboard logic

--- number/models.py
+++ number/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class LeaderboardScore(models.Model):
+    player_name = models.CharField(max_length=100)
+    attempts = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.player_name} - {self.attempts}"
+    
+    def save_score(self):
+        self.save()
